99e258859fc215b2249e2044b610ae17fed02bb3
Aabed <ahmad.aabed.m@gmail.com>
2024-02-03 13:29:23 +0200

    Update [...all].js

--- api/proxy/[...all].js
+++ api/proxy/[...all].js
@@ -1,10 +1,11 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
-module.exports = createProxyMiddleware({
+const proxy = createProxyMiddleware({
   target: 'http://18.212.250.130',
   changeOrigin: true,
-  pathRewrite: (path, req) => {
-    // Do not rewrite the path, pass it as-is
-    return path;
-  },
+  pathRewrite: (path, req) => path, // Pass the path as-is
 });
+
+module.exports = (req, res) => {
+  return proxy(req, res);
+};
